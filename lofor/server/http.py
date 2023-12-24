f8d4825bed71ef965143050c94059e147028bf60
Tej Magar <tejmagar20510@gmail.com>
2023-12-24 16:39:50 +0545

    added docs

--- lofor/server/http.py
+++ lofor/server/http.py
@@ -7,6 +7,10 @@ from lofor.manager.config import ConfigManager
 
 
 class Request:
+    """
+    A request object for storing request information.
+    """
+
     def __init__(self, raw_headers: bytes):
         self.headers = OrderedDict()
 
@@ -22,13 +26,27 @@ class Request:
             name, value = header.split(':', 1)
             self.headers[name.strip()] = value.strip()
 
-    def set(self, name, value) -> None:
+    def set_headers(self, name, value) -> None:
+        """
+        Set new header or update existing one.
+        """
+
         self.headers[name] = value
 
-    def remove(self, name) -> None:
-        del self.headers[name]
+    def remove_headers(self, name) -> None:
+        """
+        Remove headers if exists.
+        """
 
-    def build(self) -> str:
+        headers_val = self.headers.get(name)
+        if headers_val:
+            del self.headers[name]
+
+    def build_headers(self) -> str:
+        """
+        Construct request headers back to text.
+        """
+
         _headers = f'{self.method} {self.path} {self.http_version}\r\n'
         for (name, value) in self.headers.items():
             _headers += f'{name}: {value}\r\n'
@@ -37,13 +55,21 @@ class Request:
         return _headers
 
     def header_bytes(self) -> bytes:
-        return self.build().encode()
+        """
+        Headers as bytes object.
+        """
+
+        return self.build_headers().encode()
 
     def __str__(self) -> str:
-        return self.build()
+        return self.build_headers()
 
 
 def scan_headers(sock: socket.socket) -> Tuple[bytes, bytes]:
+    """
+    Scans for http headers and returns header bytes and partial body bytes if any.
+    """
+
     data = b''
 
     while True:
@@ -66,7 +92,11 @@ def scan_headers(sock: socket.socket) -> Tuple[bytes, bytes]:
     return header_bytes, body_bytes
 
 
-def read_body(client_socket, content_length: int, initial_bytes: bytes) -> bytes:
+def read_body(client_socket: socket.socket, content_length: int, initial_bytes: bytes) -> bytes:
+    """
+    Read bytes from the client socket wit the given content length specified. Return bytes with combination to initial bytes.
+    """
+
     data = initial_bytes
 
     while len(data) != content_length:
@@ -85,6 +115,11 @@ forwards: dict[str, dict] = config.get('forwards')
 
 
 def get_matching_host_config(host: str, path: str) -> Optional[dict]:
+    """
+    Returns the most matching host config for the current host and path.
+    Host level matching will have the more priority than the path matching.
+    """
+
     for (forward_rule, data) in forwards.items():
         """
         Path always starts with forward slash '/'
@@ -100,8 +135,12 @@ def get_matching_host_config(host: str, path: str) -> Optional[dict]:
     return None
 
 
-def get_host(path: str) -> Tuple[str | None, int]:
-    parsed = urlparse(path)
+def get_hostname_and_port(url: str) -> Tuple[str | None, int]:
+    """
+    Returns parsed hostname and port as Tuple.
+    """
+
+    parsed = urlparse(url)
 
     port = parsed.port
     if not parsed.port and parsed.scheme == 'https':
@@ -111,17 +150,21 @@ def get_host(path: str) -> Tuple[str | None, int]:
 
 
 def modify_request(request: Request, host: str, port: int, https: bool, is_ws: bool) -> Request:
+    """
+    Modifies request headers for forwarding.
+    """
+
     if not is_ws:
         if https:
-            request.headers['Origin'] = f'https://{host}'
+            request.set_headers('Origin', f'https://{host}')
 
         else:
-            request.headers['Origin'] = f'http://{host}:{port}'
+            request.set_headers('Origin', f'http://{host}:{port}')
 
-    request.headers['Host'] = f'{host}:{port}'
-    request.headers['Referer'] = host
+    request.set_headers('Host', f'{host}:{port}')
+    request.set_headers('Referer', host)
 
     if not is_ws:
-        request.headers['Connection'] = 'close'
+        request.set_headers('Connection', 'close')
 
     return request
--- lofor/server/serve.py
+++ lofor/server/serve.py
@@ -11,6 +11,14 @@ def start(host, port):
     server.listen()
 
     while True:
+        """
+        Spawn new thread to handle each client. Currently, keep-alive connection is not supported yet except for 
+        websocket connection. So for every new resources request like Javascript, stylesheets, 
+        new connections are created.
+        
+        For HTTP, we just close the connection after receiving data from the target server and close the connection.
+        Closing connection after sending all data tells the client, the response is completed.
+        """
         client_sock, _ = server.accept()
         thread = threading.Thread(target=handle_client, args=(client_sock,))
         thread.daemon = True
@@ -18,6 +26,10 @@ def start(host, port):
 
 
 def handle_path_not_configured(client_socket: socket.socket):
+    """
+    If path is not set, display the HTTP 404 error message with instructions.
+    """
+
     response = 'HTTP/1.1 404 NOT_FOUND\r\n'
     response += 'Content-Type: text/html\r\n'
     response += '\r\n'
@@ -32,11 +44,20 @@ def handle_path_not_configured(client_socket: socket.socket):
       </body>
     </html>
     '''
+
     client_socket.sendall(response.encode())
+
+    # Keep alive not supported. Close connection, so that server knows all the data has been received.
     client_socket.close()
 
 
 def create_socket_client(host, port, is_https: bool) -> Union[socket.socket | ssl.SSLSocket]:
+    """
+    Creates a new socket client based on the socket type.
+
+    If the target origin is https, creates a client socket with ssl support else normal socket client.
+    """
+
     client_socket = socket.create_connection((host, port))
 
     if is_https:
@@ -47,10 +68,15 @@ def create_socket_client(host, port, is_https: bool) -> Union[socket.socket | ss
     return client_socket
 
 
-def handle_receive_from_target_server(receive_from: socket.socket, client_socket: socket.socket):
+def handle_receive_from_target_server(target_socket: socket.socket, client_socket: socket.socket):
+    """
+    Forwards incoming bytes received from the target socket to the client socket.
+    Automatically closes the client socket if the target socket is closed.
+    """
+
     try:
         while True:
-            chunk = receive_from.recv(1024)
+            chunk = target_socket.recv(1024)
             if not chunk:
                 break
 
@@ -60,6 +86,11 @@ def handle_receive_from_target_server(receive_from: socket.socket, client_socket
 
 
 def handle_receive_from_request_client(client_socket: socket.socket, target_socket: socket.socket):
+    """
+    Forwards the incoming bytes received from the client (browser or any http client) to the target server.
+    Automatically closes connection to the target socket if the target client is closed.
+    """
+
     try:
         while True:
             chunk = client_socket.recv(1024)
@@ -72,25 +103,35 @@ def handle_receive_from_request_client(client_socket: socket.socket, target_sock
 
 
 def handle_client(client_socket: socket.socket):
+    """
+    Process incoming bytes received from the client and target server.
+    """
+
     raw_headers, body_part = http.scan_headers(client_socket)
     request = http.Request(raw_headers)
     original_host = request.headers['Host']
     is_websocket = request.headers.get('Upgrade', '').lower() == 'websocket'
 
+    # Search and return the most matching target host based on the client send host and pathname.
     matching_config = http.get_matching_host_config(host=original_host, path=request.path)
+
+    # Check if the path is matched or not in the config.
     if not matching_config:
+        # Display path not configured message
         handle_path_not_configured(client_socket)
         return
 
-    proxy_to = matching_config.get('proxy_to')
-    host, port = http.get_host(proxy_to)
-    https = matching_config.get('https')
+    proxy_to: str = matching_config.get('proxy_to')  # Target host
+    host, port = http.get_hostname_and_port(proxy_to)  # Host and port based on the matching host
+    https: bool = matching_config.get('https')  # True if site is https else False
 
     if not port:
         port = 443 if https else 80
 
     modified_request = http.modify_request(request, host, port, https, is_websocket)
     scheme = 'https' if https else 'http'
+
+    # Print forwarding logs
     print(f'\033[94mForwarding http://{original_host}{request.path} ==> {scheme}://{host}:{port}{request.path}\033[0m')
 
     try:
@@ -106,6 +147,7 @@ def handle_client(client_socket: socket.socket):
             forward_client.sendall(body)
 
         if is_websocket:
+            # Handle websocket protocol
             print('Starting new threads for websocket.')
             receive_from_target_server = threading.Thread(target=handle_receive_from_target_server,
                                                           args=(forward_client, client_socket))
@@ -118,6 +160,7 @@ def handle_client(client_socket: socket.socket):
             receive_from_client.start()
 
         while not is_websocket:
+            # Handle HTTP protocol
             chunk = forward_client.recv(1024)
             if not chunk:
                 forward_client.close()
@@ -128,5 +171,9 @@ def handle_client(client_socket: socket.socket):
     except socket.error:
         print('Connection closed')
 
+    # Only HTTP connection closing is handled here. Websocket connection will be not closed.
     if not is_websocket:
+        """
+        Once all the page is received from target server and sent to client, close the connection.
+        """
         client_socket.close()
